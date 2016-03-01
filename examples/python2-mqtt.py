11b895f4b1e9759703dbd053e635c8e90868534c
Brad Campbell <bradjc5@gmail.com>
2016-03-01 17:48:18 -0500

    update topic name gateway python

--- examples/python2-mqtt.py
+++ examples/python2-mqtt.py
@@ -14,7 +14,7 @@ HOST = '127.0.0.1'
 def on_connect(client, userdata, flags, rc):
 	# Subscribing in on_connect() means that if we lose the connection and
 	# reconnect then subscriptions will be renewed.
-	client.subscribe('ble-gateway-advertisements')
+	client.subscribe('gateway-data')
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
